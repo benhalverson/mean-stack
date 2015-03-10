d90088a4f52154a5978ce8d92ade03b8900f5027
Ben Halverson <benhalverson33@gmail.com>
2015-03-10 04:21:12 -0700

    client login

--- public_dir/app/account/navBarLoginCtrl.js
+++ public_dir/app/account/navBarLoginCtrl.js
@@ -1,5 +1,12 @@
-angular.module("app").controller("navBarLoginCtrl", function($scope){
+angular.module("app").controller("navBarLoginCtrl", function($scope, $http){
 	$scope.sigin = function(username, password) {
 		console.log("Not signed in");
+			$http.post("/login", {username: username, password: password}).then(function(response){
+				if(response.data.success){
+					console.log("logged in");
+				} else {
+					console.log("failed to log in!");
+				}
+			})
 	}
 });
