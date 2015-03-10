1ea2ee87a39dfb38505d28399da9ad4414b2ddfb
Ben Halverson <benhalverson33@gmail.com>
2015-03-10 04:12:41 -0700

    passport setup on server

--- server/config/express.js
+++ server/config/express.js
@@ -2,6 +2,9 @@ var express = require("express");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
 var stylus = require("stylus");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var passport = require("passport");
 
 module.exports = function(app, config){
 	function compile(str, path) {
@@ -11,10 +14,14 @@ module.exports = function(app, config){
 	app.set("views", config.rootPath + "/server/views");
 	app.set("view engine", "jade");
 	app.use(logger("dev"));
+	app.use(cookieParser());
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({
 		extended: true
 	}));
+	app.use(session({secret: "secret"}));
+	app.use(passport.initialize());
+	app.use(passport.session());
 	app.use(stylus.middleware({
 				src: config.rootPath + "/public_dir",
 				compile: compile
